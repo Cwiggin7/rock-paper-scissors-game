ad5f4a40af575eda65b6ebb225ab28802b098584
Connor <cwiggin7@ksu.edu>
2022-02-20 03:23:04 -0600

    game functionality

--- script.js
+++ script.js
@@ -4,3 +4,75 @@ document.getElementById('start-button').addEventListener('click', () => {
     document.getElementById('starter-container').remove()
     gameContainer.style.display = 'flex'
 })
+
+class Game {
+    playerScore = 0
+    computerScore = 0
+    
+    constructor(playerScoreDisplay, computerScoreDisplay, resultsText) {
+        this.playerScoreDisplay = playerScoreDisplay
+        this.computerScoreDisplay = computerScoreDisplay
+        this.resultsText = resultsText
+    }
+
+    clear() {
+        this.playerScore = 0
+        this.computerScore = 0
+        playerScoreDisplay.innerText = playerScoreDisplay.innerText.slice(0, -1) + '0'
+        computerScoreDisplay.innerText = computerScoreDisplay.innerText.slice(0, -1) + '0'
+    }
+
+    playRound(playerChoice) {
+        const computerChoice = this.computerChoose()
+        if (computerChoice === playerChoice) {
+            resultsText.innerText = 'Tie round!'
+        }
+        else if ((computerChoice === 'rock' && playerChoice === 'scissors') ||
+                (computerChoice === 'paper' && playerChoice === 'rock') ||
+                (computerChoice === 'scissors' && playerChoice === 'paper')) {
+                    resultsText.innerText = `Computer won this round. ${computerChoice} beats ${playerChoice}`
+                    this.computerScore++
+                    computerScoreDisplay.innerText = computerScoreDisplay.innerText.slice(0, -1) + this.computerScore
+                }
+        else {
+            resultsText.innerText = `You won this round! ${playerChoice} beats ${computerChoice}`
+            this.playerScore++
+            playerScoreDisplay.innerText = playerScoreDisplay.innerText.slice(0, -1) + this.playerScore
+        }
+        this.checkForWin()
+    }
+
+    computerChoose() {
+        let choices = ['rock', 'paper', 'scissors']
+        return choices[~~(choices.length * Math.random())]
+    }
+
+    checkForWin() {
+        if (this.playerScore === 5) {
+            resultsText.innerText = 'You won the game!'
+            this.clear()
+        }
+        else if (this.computerScore === 5) {
+            resultsText.innerText = 'The computer won the game - better luck next time'
+            this.clear()
+        }
+    }
+}
+
+const playerScoreDisplay = document.getElementById('player-score-display')
+const computerScoreDisplay = document.getElementById('computer-score-display')
+const resultsText = document.getElementById('results-text')
+
+const game = new Game(playerScoreDisplay, computerScoreDisplay, resultsText)
+
+document.getElementById('rock').addEventListener('click', () => {
+    game.playRound('rock')
+})
+
+document.getElementById('paper').addEventListener('click', () => {
+    game.playRound('paper')
+})
+
+document.getElementById('scissors').addEventListener('click', () => {
+    game.playRound('scissors')
+})
